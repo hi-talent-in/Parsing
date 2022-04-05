65fea5201b3e5278b3b01ff8b8bac413a780cbf5
milind01 <milindpathare932001@gmail,com>
2022-04-05 11:39:31 +0530

    Inital commit

---
+++ src/main/java/com/code/BankStatementDetail.java
@@ -0,0 +1,26 @@
+package com.code;
+
+public class BankStatementDetail {
+
+	
+	
+	//To Do Part.
+	
+	
+	/*
+	 * public string Nickname {get; set;}
+	 * 
+	 * public string AccountNumber {get; set;}
+	 * 
+	 * ..... other property as required
+	 * 
+	 * //This is just a sample example, u will need something more sophisticated
+	 * method
+	 * 
+	 * public string override ToString(){
+	 * 
+	 * return Nickname +" "+ AccountNumber+ " ";
+	 */
+
+
+}
--- src/main/java/com/code/Code.java
+++ src/main/java/com/code/Code.java
@@ -7,14 +7,31 @@ import org.apache.pdfbox.pdfparser.PDFParser;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
 
+
+
+
+
 public class Code {
 
 	public static void main(String[] args) throws IOException {
 		
 		
+		String data = readdata();
+		 System.out.println(data);
+	
+		BankStatementDetail BSD = getBankStatementDetail(data);
+	
+	
+		//TO DO PART. 
+		
+		
+
+	}
+	
+	public static String readdata()  throws IOException {
+		
 		File file = new File("himanshi dec 21.pdf");
 	
-		
 		PDFParser parser =new PDFParser(new org.apache.pdfbox.io.RandomAccessFile(file, "r"));
 		parser.parse();
 		
@@ -23,8 +40,24 @@ public class Code {
 		
 		PDFTextStripper str = new PDFTextStripper();
 		String data = str.getText(pddoc);
-		System.out.println(data);
-
+		return data;
+		
+	
+	}
+	
+	public static BankStatementDetail getBankStatementDetail(String data) throws IOException {
+		
+		BankStatementDetail BSD = new 	BankStatementDetail();
+	
+		
+		//To Do Part.
+		// Populate the Bank statement details from Data.
+		
+		
+		return BSD;
+		
+	
+		
 	}
 
 }
