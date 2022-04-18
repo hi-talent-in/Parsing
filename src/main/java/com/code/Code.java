db88d6d313024ea834db7fbaa9fa30d4a4e97d80
Chandan <chandan.a.shinde@gmail.com>
2022-04-18 18:43:45 +0530

    Made changes to the existing file to incorporate the requirements of the assessment.

--- src/main/java/com/code/Code.java
+++ src/main/java/com/code/Code.java
@@ -1,30 +1,89 @@
-package com.code;
-
-import java.io.File;
-import java.io.IOException;
-import org.apache.pdfbox.cos.COSDocument;
-import org.apache.pdfbox.pdfparser.PDFParser;
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.text.PDFTextStripper;
-
 public class Code {
 
 	public static void main(String[] args) throws IOException {
-		
-		
+
+
 		File file = new File("himanshi dec 21.pdf");
-	
-		
+
+
 		PDFParser parser =new PDFParser(new org.apache.pdfbox.io.RandomAccessFile(file, "r"));
 		parser.parse();
-		
+
 		COSDocument cosdoc = parser.getDocument();
-		PDDocument pddoc = new PDDocument(cosdoc);		
-		
+		PDDocument pddoc = new PDDocument(cosdoc);
+
 		PDFTextStripper str = new PDFTextStripper();
 		String data = str.getText(pddoc);
 		System.out.println(data);
 
+		String pname;
+		String line;
+
+		Scanner obj = new Scanner(data);
+		AccountDetails ad = new AccountDetails();
+		while(obj.hasNextLine()) {
+			line = obj.nextLine();
+			line.trim();
+			if(line.startsWith("Payee Name")) {
+				int i = line.lastIndexOf(':');
+				ad.setPayeeName(line.substring(i+1).trim());
+			}
+			else if(line.startsWith("Nickname")) {
+				int i = line.lastIndexOf(':');
+				ad.setNickName(line.substring(i+1).trim());
+			}
+			else if(line.startsWith("Credit Account No")) {
+				int i = line.lastIndexOf(':');
+
+				try {
+					String s = line.substring(i+1).trim();
+					BigInteger num = new BigInteger(s);
+					ad.setCreditAccNum(num);
+				}
+				catch (NumberFormatException ex){
+		            ex.printStackTrace();
+		        }
+			}
+			else if(line.startsWith("Remarks")) {
+				int i = line.lastIndexOf(':');
+				//System.out.println("**"+line.substring(i+1).trim()+"**");
+				ad.setRemarks(line.substring(i+1).trim());
+			}
+			else if(line.startsWith("Debit Account")) {
+				int i = line.lastIndexOf(':');
+
+				try {
+					String s = line.substring(i+1).trim();
+					BigInteger num = new BigInteger(s);
+					ad.setDebitAccNum(num);
+				}
+				catch (NumberFormatException ex){
+		            ex.printStackTrace();
+		        }
+			}
+			else if(line.startsWith("Date")) {
+				int i = line.lastIndexOf(':');
+				ad.setDate(line.substring(i+1).trim());
+			}
+			else if(line.startsWith("Amount")) {
+				int i = line.lastIndexOf(':');
+				String s = line.substring(i+1).trim();
+				ad.setAmount(line.substring(i+1).trim());
+			}
+			else if(line.startsWith("Payment Type")) {
+				int i = line.lastIndexOf(':');
+				ad.setPaymentType(line.substring(i+1).trim());
+			}
+			else if(line.startsWith("Pay via")) {
+				int i = line.lastIndexOf(':');
+				//System.out.println(line.substring(i+1).trim());
+				ad.setPayVia(line.substring(i+1).trim());
+			}
+		}
+		System.out.println("Parsed Output :");
+		System.out.println(ad.toString());
+
+
 	}
 
-}
+	}
